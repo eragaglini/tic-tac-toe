0de71299ad598742b17ffa274a0c1208f2e5db3d
eragaglini <eragaglini7@gmail.com>
2021-12-31 20:33:11 +0100

    updated script.js to redefine playerWon function

--- minimax.js
+++ minimax.js
@@ -3,43 +3,80 @@
 // https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
 // https://youtu.be/I64-UTORVfU
 // https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
-
 function bestMove() {
     // AI to make its turn
     let bestScore = -Infinity;
     let move;
     for (let i = 0; i < spaces.length; i++) {
-      for (let j = 0; j < spaces[i].length; j++) {
-        // Is the spot available?
-        if (spaces[i][j] == null) {
-          spaces[i][j] = currentPlayer;
-          let score = minimax(spaces, 0, true);
-          spaces[i][j] = null;
-          if (score > bestScore) {
-            bestScore = score;
-            move = { i, j };
-          }
+        for (let j = 0; j < spaces[i].length; j++) {
+            // Is the spot available?
+            if (spaces[i][j] == null) {
+                //spaces[i][j] = currentPlayer;
+                let score = minimax(spaces,0, true);
+                //spaces[i][j] = null;
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = {
+                        i,
+                        j
+                    };
+                }
+            }
         }
-      }
     }
     return move;
-  }
-  
-  
-  function minimax(spaces, depth, isMaximizing) {
-    //console.log(spaces);
+}
+
+let scores = {
+    X: 10,
+    O: -10
+  };
+
+
+function minimax(spaces,depth, isMaximizing) {
+    //return 1;
+    //console.log(depth);
+    // uscita dalla funzione
     let result = playerWon();
     if (result) {
-        if (currentPlayer == tick_x) {
-            return 10;
-        }
-        else {
-            return -10;
-        }
+        console.log(scores[result]);
+        return scores[result];
     }
+
     result = playerDraw();
-    if (result) {
+    if (result === 'tie') {
         return 0;
     }
-    return 1;
-  }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < spaces.length; i++) {
+            for (let j = 0; j < spaces[i].length; j++) {
+                // Is the spot available?
+                if (spaces[i][j] == null) {
+                    spaces[i][j] = tick_x;
+                    //console.log(spaces);
+                    let score = minimax(spaces, depth + 1, false);
+                    spaces[i][j] = null;
+                    bestScore = Math.max(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < spaces.length; i++) {
+            for (let j = 0; j < spaces[i].length; j++) {
+                // Is the spot available?
+                if (spaces[i][j] == null) {
+                    spaces[i][j] = tick_circle;
+                    console.log(spaces);
+                    let score = minimax(spaces, depth + 1, false);
+                    spaces[i][j] = null;
+                    bestScore = Math.min(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+}
--- script.js
+++ script.js
@@ -10,163 +10,175 @@ const tick_x = 'X';
 let currentPlayer = tick_circle;
 
 const drawBoard = (size) => {
-  for (let i = 0; i < size; i++) {
-    var row = document.createElement("div");
-    row.id = i;
-    board.appendChild(row);
-    spaces[i] = [];
-    for (let j = 0; j < size; j++) {
-      var input = document.createElement("input");
-      input.id = j;
-      input.readOnly = true;
-      input.type = "text";
-      input.setAttribute('class', 'box'); 
-      row.appendChild(input);
-      spaces[i][j] = null;
+    for (let i = 0; i < size; i++) {
+        var row = document.createElement("div");
+        row.id = i;
+        board.appendChild(row);
+        spaces[i] = [];
+        for (let j = 0; j < size; j++) {
+            var input = document.createElement("input");
+            input.id = j;
+            input.readOnly = true;
+            input.type = "text";
+            input.setAttribute('class', 'box');
+            row.appendChild(input);
+            spaces[i][j] = null;
+        }
     }
-  }
-  const boxes = document.querySelectorAll('.box');
-  boxes.forEach((box, i) => {
-    box.addEventListener('click', boxClicked);
-  });
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box, i) => {
+        box.addEventListener('click', boxClicked);
+    });
 };
 
 const resetBoard = () => {
-  while (board.firstChild) {
-    board.removeChild(board.firstChild);
-  }
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
 };
 
 const boxClicked = (e) => {
-  let gameOver = playerMove(e.target);
-  ai_move = bestMove();
-  if (ai_move) {
-    row = board.childNodes[ai_move.i];
-    target = row.childNodes[ai_move.j];
-  }
-  if (!gameOver) {  
-    playerMove(target);
-  }
+    let gameOver = playerMove(e.target);
+    ai_move = bestMove();
+    if (ai_move) {
+        row = board.childNodes[ai_move.i];
+        target = row.childNodes[ai_move.j];
+    }
+    if (!gameOver) {
+        playerMove(target);
+    }
 };
 
 const playerMove = (target) => {
-  target.removeEventListener("click", boxClicked , false);
-  const row_id = target.parentNode.id;
-  const id = target.id;
-  if (!spaces[row_id][id]) {
-    spaces[row_id][id] = currentPlayer;
-    target.value = currentPlayer;
-    if (playerWon()) { 
-      text.innerText = `${currentPlayer} has won!`;
-      
-      var x = document.getElementById("restart");
-      x.style.display = x.style.display === 'none' ? '' : 'none';
-      setTimeout(function name(params) {
-        text.innerText = `Play`;
-        var x = document.getElementById("board");
-        x.style.display = x.style.display === 'none' ? '' : 'none';
-        var x = document.getElementById("menu");
-        x.style.display = x.style.display === 'none' ? '' : 'none';
-        var x = document.getElementById("restart");
-        x.style.display = x.style.display === 'none' ? '' : 'none';
-      }, 1000)
-      return true;
-    }
+    target.removeEventListener("click", boxClicked, false);
+    const row_id = target.parentNode.id;
+    const id = target.id;
+    if (!spaces[row_id][id]) {
+        spaces[row_id][id] = currentPlayer;
+        target.value = currentPlayer;
+        if (playerWon()) {
+            text.innerText = `${currentPlayer} has won!`;
 
-    if (playerDraw()) {
-      text.innerText = `It's a draw!!`;
-      
-      var x = document.getElementById("restart");
-      x.style.display = x.style.display === 'none' ? '' : 'none';
-      setTimeout(function name(params) {
-        text.innerText = `Play`;
-        var x = document.getElementById("board");
-        x.style.display = x.style.display === 'none' ? '' : 'none';
-        var x = document.getElementById("menu");
-        x.style.display = x.style.display === 'none' ? '' : 'none';
-        var x = document.getElementById("restart");
-        x.style.display = x.style.display === 'none' ? '' : 'none';
-      }, 1000)
-      return true;
+            var x = document.getElementById("restart");
+            x.style.display = x.style.display === 'none' ? '' : 'none';
+            setTimeout(function name(params) {
+                text.innerText = `Play`;
+                var x = document.getElementById("board");
+                x.style.display = x.style.display === 'none' ? '' : 'none';
+                var x = document.getElementById("menu");
+                x.style.display = x.style.display === 'none' ? '' : 'none';
+                var x = document.getElementById("restart");
+                x.style.display = x.style.display === 'none' ? '' : 'none';
+            }, 1000)
+            return true;
+        }
+
+        if (playerDraw()) {
+            console.log(playerDraw());
+            text.innerText = `It's a draw!!`;
+
+            var x = document.getElementById("restart");
+            x.style.display = x.style.display === 'none' ? '' : 'none';
+            setTimeout(function name(params) {
+                text.innerText = `Play`;
+                var x = document.getElementById("board");
+                x.style.display = x.style.display === 'none' ? '' : 'none';
+                var x = document.getElementById("menu");
+                x.style.display = x.style.display === 'none' ? '' : 'none';
+                var x = document.getElementById("restart");
+                x.style.display = x.style.display === 'none' ? '' : 'none';
+            }, 1000)
+            return true;
+        }
+        currentPlayer = currentPlayer === tick_circle ? tick_x : tick_circle;
     }
-    currentPlayer = currentPlayer === tick_circle ? tick_x : tick_circle;
-  }
 };
 
+
 const playerWon = () => {
-  const allEqual = arr => arr.every( v => v === currentPlayer )
+    //console.log(spaces);
+    //const allEqual = arr => arr.every(v => v === currentPlayer)
+    const allEqual = arr => arr.length && arr.reduce(function(a, b){return (a === b)?a:false;}) === arr[0];
 
-  for (let index = 0; index < spaces.length; index++) {
-    if (allEqual(spaces[index])) {
-      return true;
+    for (let index = 0; index < spaces.length; index++) {
+        if (allEqual(spaces[index]) && spaces[index][0] !== null) {
+            //console.log(spaces[index][0]);
+            console.log(spaces)
+            return spaces[index][0];
+        }
     }
-  }
 
-  const arrayColumn = (arr, n) => arr.map(x => x[n]);
-  for (let index = 0; index < spaces[0].length; index++) {
-    if (allEqual(arrayColumn(spaces, index))) {
-      return true;
+    const arrayColumn = (arr, n) => arr.map(x => x[n]);
+    for (let index = 0; index < spaces[0].length; index++) {
+        let column = arrayColumn(spaces, index);
+        if (allEqual(column) && column[0] !== null) {
+          console.log(column[0]);
+          return column[0];
+        }
     }
-  }
-  
 
-  let diagonalArray = [];
-  for (let i = 0; i < spaces.length; i++) {
-    for (let j = 0; j < spaces.length; j++) {
-      if (i == j) {
-        diagonalArray.push(spaces[i][j]);
-      }
-    }
-  }
-  if (allEqual(diagonalArray)) {
-    // return true;
-    return {'currentPlayer': currentPlayer, 'win': true};
-  }
 
-  diagonalArray = [];
-  for (let i = 0; i < spaces.length; i++) {
-    for (let j = 0; j < spaces.length; j++) {
-      if ((i + j) == (spaces.length - 1)) {
-        diagonalArray.push(spaces[i][j]);
-      }
+    let diagonalArray = [];
+    for (let i = 0; i < spaces.length; i++) {
+        for (let j = 0; j < spaces.length; j++) {
+            if (i == j) {
+                diagonalArray.push(spaces[i][j]);
+            }
+        }
     }
-  }
-  if (allEqual(diagonalArray)) {
-    return true;
-  }
+    if (allEqual(diagonalArray) && diagonalArray[0] !== null) {
+        // return true;
+        console.log(diagonalArray[0])
+        return diagonalArray[0];
+    }
+
+    diagonalArray = [];
+    for (let i = 0; i < spaces.length; i++) {
+        for (let j = 0; j < spaces.length; j++) {
+            if ((i + j) == (spaces.length - 1)) {
+                diagonalArray.push(spaces[i][j]);
+            }
+        }
+    }
+    if (allEqual(diagonalArray)  && diagonalArray[0] !== null) {
+        console.log(diagonalArray[0]);
+        return diagonalArray[0];
+    }
+
+    return false;
 };
 
 const playerDraw = () => {
-  let draw = [];
-  for(var i = 0; i < spaces.length; i++) {
-    for(var j = 0; j < spaces.length; j++) {
-      draw.push(spaces[i][j]);
+    let draw = [];
+    for (var i = 0; i < spaces.length; i++) {
+        for (var j = 0; j < spaces.length; j++) {
+            draw.push(spaces[i][j]);
+        }
     }
-  }
-  if(draw.every(element => element !== null)) {
-    return true;
-  }
+    if (draw.every(element => element !== null)) {
+        return 'tie';
+    }
 };
 
 const restart = () => {
-  resetBoard();
-  currentPlayer = tick_circle;
-  spaces = [];
-  spaces.forEach((space, i) => {
-    spaces[i] = null;
-  });
-  boxes.forEach((box) => {
-    box.value = '';
-  });
-  text.innerText = `Play`;
-  
-  // show the grid and remove the menu
-  var x = document.getElementById("menu");
-  x.style.display = x.style.display === 'none' ? '' : 'none';
-  var x = document.getElementById("board");
-  x.style.display = x.style.display === 'none' ? '' : 'none';
-  var size = parseInt( document.getElementById('size').value );
-  drawBoard(size);
+    resetBoard();
+    currentPlayer = tick_circle;
+    spaces = [];
+    spaces.forEach((space, i) => {
+        spaces[i] = null;
+    });
+    boxes.forEach((box) => {
+        box.value = '';
+    });
+    text.innerText = `Play`;
+
+    // show the grid and remove the menu
+    var x = document.getElementById("menu");
+    x.style.display = x.style.display === 'none' ? '' : 'none';
+    var x = document.getElementById("board");
+    x.style.display = x.style.display === 'none' ? '' : 'none';
+    var size = parseInt(document.getElementById('size').value);
+    drawBoard(size);
 };
 restartBtn.addEventListener('click', restart);
 board.style.display = board.style.display === 'none' ? '' : 'none';
@@ -179,76 +191,76 @@ var x, i, j, l, ll, selElmnt, a, b, c;
 x = document.getElementsByClassName("custom-select");
 l = x.length;
 for (i = 0; i < l; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
-  /* For each element, create a new DIV that will act as the selected item: */
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 0; j < ll; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
-        var y, i, k, s, h, sl, yl;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        sl = s.length;
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < sl; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
-            for (k = 0; k < yl; k++) {
-              y[k].removeAttribute("class");
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /* For each element, create a new DIV that will act as the selected item: */
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /* For each element, create a new DIV that will contain the option list: */
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 0; j < ll; j++) {
+        /* For each option in the original select element,
+        create a new DIV that will act as an option item: */
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function(e) {
+            /* When an item is clicked, update the original select box,
+            and the selected item: */
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    yl = y.length;
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
             }
-            this.setAttribute("class", "same-as-selected");
-            break;
-          }
-        }
-        h.click();
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function(e) {
+        /* When the select box is clicked, close any other select boxes,
+        and open/close the current select box: */
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
     });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-  });
 }
 
 function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-  except the current select box: */
-  var x, y, i, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
+    /* A function that will close all select boxes in the document,
+    except the current select box: */
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
     }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
     }
-  }
 }
 
 /* If the user clicks anywhere outside the select box,
