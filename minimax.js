bfff4880257faa155517813c416ce757143f3a56
eragaglini <eragaglini7@gmail.com>
2022-01-09 20:27:51 +0100

    improved minimax algorithm to manage move depth

--- minimax.js
+++ minimax.js
@@ -4,58 +4,61 @@ function bestMove() {
 
 function minimax(spaces, depth, isMaximizing) {
     
-    let win = playerWon();
+    var win = playerWon();
     if (win === 'X') {
-        return { score: 10 };
+        return { score: 10, depth: depth };
     }
     if (win === 'O') {
-        return { score: -10 };
+        return { score: -10, depth:depth };
     }
 
     draw = playerDraw();
     if (draw === 'tie') {
-        return { score: 0 };
+        return { score: 0, depth: depth };
     }
-    let moves = [];
-    let move = {};
+    var moves = [];
+    var move = {};
 
     for (let i = 0; i < spaces.length; i++) {
         for (let j = 0; j < spaces[i].length; j++) {
             if (spaces[i][j] === null) {
+                move.index = {i: i, j: j};
                 if (isMaximizing) {
-                    move.index = {i: i, j: j};
                     spaces[i][j] = tick_x;
-                    let result = minimax(spaces, depth + 1, false);
+                    var result = minimax(spaces, depth + 1, false);
                     spaces[i][j] = null;
                     move.score = result.score;
                 } else {
-                    move.index = {i: i, j: j};
                     spaces[i][j] = tick_circle;
-                    let result = minimax(spaces, depth + 1, true);
-                    spaces[i][j] = null;
-                    move.score = result.score;
+                    var result = minimax(spaces, depth + 1, true);
                 }
+                spaces[i][j] = null;
+                move.score = result.score;
+                move.depth = result.depth;
                 //moves.push(move);
-                moves.push({index: move.index, score: move.score});
+                moves.push({index: move.index, score: move.score, depth: move.depth});
             }
         }
     }
 
     var bestMove;
+    var minimum_depth = Infinity;
     if (isMaximizing) {
         var bestScore = -Infinity;
         for (var i = 0; i < moves.length; i++) {
-            if (moves[i].score > bestScore) {
+            if (moves[i].score > bestScore || (moves[i].score == bestScore && moves[i].depth < minimum_depth)) {
                 bestScore = moves[i].score;
                 bestMove = i;
+                minimum_depth = moves[i].depth;
             }
         }
     } else {
         var bestScore = Infinity;
         for (var i = 0; i < moves.length; i++) {
-            if (moves[i].score < bestScore) {
+            if (moves[i].score < bestScore || (moves[i].score == bestScore && moves[i].depth < minimum_depth))  {
                 bestScore = moves[i].score;
                 bestMove = i;
+                minimum_depth = moves[i].depth;
             }
         }
     }
