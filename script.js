9e401cb78df7d84e2a5aeab41ac7a5784972dd33
eragaglini <eragaglini7@gmail.com>
2022-02-12 14:56:17 +0100

    edited and removed dropdown menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const boxes = document.querySelectorAll('.box');
 const text = document.querySelector('#heading');
-const restartBtn = document.querySelector('#restart');
+//const option = document.querySelector('.options');
 const board = document.querySelector('#board');
+
+const option_three = document.querySelector('#three');
+const option_five = document.querySelector('#five');
+const option_seven = document.querySelector('#seven');
 
 //const spaces = [];
 let spaces = [];
@@ -59,15 +63,11 @@ const playerMove = (target) => {
         if (playerWon()) {
             text.innerText = `${currentPlayer} has won!`;
 
-            var x = document.getElementById("restart");
-            x.style.display = x.style.display === 'none' ? '' : 'none';
             setTimeout(function name(params) {
                 text.innerText = `Play`;
                 var x = document.getElementById("board");
                 x.style.display = x.style.display === 'none' ? '' : 'none';
                 var x = document.getElementById("menu");
-                x.style.display = x.style.display === 'none' ? '' : 'none';
-                var x = document.getElementById("restart");
                 x.style.display = x.style.display === 'none' ? '' : 'none';
             }, 1000)
             return true;
@@ -75,16 +75,12 @@ const playerMove = (target) => {
 
         if (playerDraw()) {
             text.innerText = `It's a draw!!`;
-
-            var x = document.getElementById("restart");
-            x.style.display = x.style.display === 'none' ? '' : 'none';
+            
             setTimeout(function name(params) {
                 text.innerText = `Play`;
                 var x = document.getElementById("board");
                 x.style.display = x.style.display === 'none' ? '' : 'none';
                 var x = document.getElementById("menu");
-                x.style.display = x.style.display === 'none' ? '' : 'none';
-                var x = document.getElementById("restart");
                 x.style.display = x.style.display === 'none' ? '' : 'none';
             }, 1000)
             return true;
@@ -155,7 +151,8 @@ const playerDraw = () => {
     }
 };
 
-const restart = () => {
+const restart = (size) => {
+    console.log(size);
     resetBoard();
     currentPlayer = tick_circle;
     spaces = [];
@@ -172,92 +169,16 @@ const restart = () => {
     x.style.display = x.style.display === 'none' ? '' : 'none';
     var x = document.getElementById("board");
     x.style.display = x.style.display === 'none' ? '' : 'none';
-    var size = parseInt(document.getElementById('size').value);
+    //var size = parseInt(document.getElementById('size').value);
     drawBoard(size);
 };
-restartBtn.addEventListener('click', restart);
+option_three.addEventListener('click', function(){
+    restart(3);
+});
+option_five.addEventListener('click', function(){
+    restart(5);
+});
+option_seven.addEventListener('click', function(){
+    restart(7);
+});
 board.style.display = board.style.display === 'none' ? '' : 'none';
-
-
-// styling for the select menu
-
-var x, i, j, l, ll, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    ll = selElmnt.length;
-    /* For each element, create a new DIV that will act as the selected item: */
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 0; j < ll; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
-            var y, i, k, s, h, sl, yl;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            sl = s.length;
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < sl; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    yl = y.length;
-                    for (k = 0; k < yl; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-    });
-}
-
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    var x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < xl; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-        }
-    }
-}
-
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
